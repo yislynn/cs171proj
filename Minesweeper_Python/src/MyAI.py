d5b72fbcbe1b41d83217b086ddea7fddcfe6bbfe
izazizan <faizadraws@gmail.com>
2022-05-04 15:29:54 -0700

    started on getAction

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -15,9 +15,8 @@
 from AI import AI
 from Action import Action
 
-global board
-global currX
-global currY
+
+
 
 class MyAI( AI ):
 
@@ -29,10 +28,12 @@ class MyAI( AI ):
 		self.__rows = rowDimension
 		self.__cols = colDimension
 		self.__totalMines = totalMines
-		self.__startX = startX
-		self.__startY = startY
-		self.__endAt = (self.__rows * self.__cols) - self.__totalMines
-		########################################################################
+		self.__currX = startX
+		self.__currY = startY
+		self.__numCovered = (self.__rows * self.__cols) - self.__totalMines
+		# int label, -1 = covered, 0-8 = label
+		self.__board = [[-1 for x in range(colDimension)] for y in range(rowDimension)]
+	########################################################################
 		#							YOUR CODE ENDS							   #
 		########################################################################
 
@@ -42,7 +43,42 @@ class MyAI( AI ):
 		########################################################################
 		#							YOUR CODE BEGINS						   #
 		########################################################################
-		return Action(AI.Action.LEAVE)
+		# update board
+		if number != -1:
+			self.__board[self.__currX][self.__currY] = number
+		# check if we're done
+		if self.__numCovered == self.__totalMines:
+			return Action(AI.Action.LEAVE)
+		# figure out UNCOVER(X,Y)
+		neighbors = self.getNeighbors(self.__currX, self.__currY)
+		# todo: try simple rule of thumb
+		# uncover all neighbors if 0
+		if number == 0:
+			for n in neighbors:
+				# uncover if covered
+				if self.__board[n[0], n[1]] == -1:
+					return Action(Action.UNCOVER, n[0], n[1])
+		# flag all if label == unmarked neighbors
+
+
+
+		# if doesn't work, use better logic
+		# if doesn't work, use EVEN BETTER logic
+
+		# if uncover, decrement numCovered
 		########################################################################
 		#							YOUR CODE ENDS							   #
 		########################################################################
+
+	def getNeighbors(self, x, y):
+		neighbors = [(x - 1, y - 1), (x - 1, y), (x - 1, y + 1),
+					 (x, y - 1), (x, y + 1),
+					 (x + 1, y - 1), (x + 1, y), (x + 1, y + 1)]
+		for n in neighbors:
+			# check if in bounds
+			if n[0] < 0 or n[0] >= self.__rows:
+				neighbors.remove(n)
+			if n[1] < 0 or n[1] >= self.__cols:
+				neighbors.remove(n)
+				# check if covered
+		return neighbors
